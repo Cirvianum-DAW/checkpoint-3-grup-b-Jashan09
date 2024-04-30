71443f8b8b91f37e4ef00558ec3d339c075af08e
AaronLunaa <aaron.lunacirvianum.cat>
2024-04-30 17:55:42 +0200

    6è commit

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,29 +1,76 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
 
-const BASE_URL_1 = 'https://pokeapi.co/api/v2/pokemon/';
-
-
-console.log("helfed");
-
 async function request(endpoint) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${endpoint}`);
-    console.log(endpoint);
     return response.json();
 }
   
-
 async function ficaUnIdPelPokemon(id) {
     const pokemonObj = await request(id);
-    console.log(pokemonObj);
     return pokemonObj;
 }
-  
+
+async function getDescAbility(url) {
+    const response = await fetch(url);
+    return response.json();
+}
+
+let thisDescUrl = [];
+let counter = 0;
+
+function escriuElPoke(myPokemonObj){
+
+    thisDescUrl = [];
+    counter = 0;
+
+    let myName = document.querySelector("#name");
+    let myHeight = document.querySelector("#height");
+    let myWeight = document.querySelector("#weight");
+
+    let mySprite = document.querySelector("img"); //ES UN ELEMENT IMG
+
+    myName.textContent = myPokemonObj.name;
+
+    myHeight.textContent = "Height: " + myPokemonObj.height;
+    myWeight.textContent = "Weight: " + myPokemonObj.weight;
+
+    mySprite.src = myPokemonObj.sprites.front_shiny
+
+    let textAbilities = "";
+
+    let myAbilitiesArray = document.querySelector("ul"); //ES UNA LLISTA 
+
+
+    myPokemonObj.abilities.forEach(ability => {
+        let newLi = document.createElement("li");
+
+        let nouH4 = document.createElement("h4");
+        let nouP = document.createElement("p");
+        
+        nouH4.textContent = ability.ability.name;
+
+        thisDescUrl =  ability.ability.url;
+        
+
+        newLi.appendChild(nouH4);
+        newLi.appendChild(nouP);
+        myAbilitiesArray.appendChild(newLi);
+
+    });
+
+}
+
 
 
 main = async () => {
 
-    const exercici1 = await ficaUnIdPelPokemon(3);
-    console.log(exercici1);
-  
-   
+    const myPokemonObj = await ficaUnIdPelPokemon(3);
+    console.log(myPokemonObj);  
+
+    escriuElPoke(myPokemonObj);   
+
+
 };
+
+
+main();
