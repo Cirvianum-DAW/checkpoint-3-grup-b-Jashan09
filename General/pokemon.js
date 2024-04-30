e7f2ce2ebe452768a13d1e82e222d392c9a1898f
AaronLunaa <aaron.lunacirvianum.cat>
2024-04-30 17:06:40 +0200

    5è commit

--- General/pokemon.js
+++ General/pokemon.js
@@ -1,1 +1,29 @@
 // Afegeix aquí el codi de JS per a la pàgina pokemon.html
+
+const BASE_URL_1 = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+console.log("helfed");
+
+async function request(endpoint) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${endpoint}`);
+    console.log(endpoint);
+    return response.json();
+}
+  
+
+async function ficaUnIdPelPokemon(id) {
+    const pokemonObj = await request(id);
+    console.log(pokemonObj);
+    return pokemonObj;
+}
+  
+
+
+main = async () => {
+
+    const exercici1 = await ficaUnIdPelPokemon(3);
+    console.log(exercici1);
+  
+   
+};
